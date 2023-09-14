2c6cf6ff4904c4e163ed10436b7a7fc68603decf
john0 <jcampusano@k3d.do>
2023-09-13 22:30:12 -0400

    Add bar chart

--- src/pages/Chart.js
+++ src/pages/Chart.js
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { HubConnectionBuilder } from '@microsoft/signalr';
-import { PieChart, Pie, Cell, Legend, Tooltip } from 'recharts';
+import { PieChart, Pie, Cell, Legend, Tooltip, BarChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
 
-const COLORS = ['#0088FE', '#74c47a', "#C23237"];
-
+const COLORS = ['#74c47a', "#C23237"];
+let totalVoters = 0;
 const transformData = (data) => {
-  console.log("Datos recibidos:", data); // Verifica qué datos estás recibiendo
+  console.log("Datos recibidos:", data);
   const votesEffectedLength = data.filter(person => person.isRegistered).length;
   const registeredVotings = data.length;
   const votesEffected = votesEffectedLength;
   const remainingVotes = data.length - votesEffectedLength;
-
+  totalVoters = registeredVotings
   console.log("Registrados:", registeredVotings);
   console.log("Votos efectuados:", votesEffected);
   console.log("Votos restantes:", remainingVotes)
 
   return [
-    { name: 'Cantidad de votantes: '+ registeredVotings, value: registeredVotings },
-    { name: 'Votos efectuados: ' + votesEffected , value: votesEffected },
+    
+    { name: 'Votos efectuados: ' + votesEffected, value: votesEffected },
     { name: 'Votos restantes: ' + remainingVotes, value: remainingVotes },
   ];
 };
+
 function Chart() {
   const baseUrl = "https://localhost:44361/votingsStream";
-  const [data, setData] = useState([]); // Inicializa data como un arreglo vacío
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const connection = new HubConnectionBuilder()
@@ -34,8 +35,6 @@ function Chart() {
     connection.start()
       .then(() => {
         console.log("Connection established");
-
-        // Invoke method and wait for response
         connection.invoke('GetDataFromDb')
           .catch(error => {
             console.error("Error invoking method:", error);
@@ -47,12 +46,9 @@ function Chart() {
 
     connection.on("Data", function (person) {
       console.log("Received data from server:", person);
-
-      // Actualiza el estado con los datos recibidos
       setData(person);
     });
 
-    // Limpia la conexión cuando el componente se desmonta
     return () => {
       connection.stop()
         .then(() => {
@@ -62,31 +58,65 @@ function Chart() {
           console.error("Error stopping connection:", error);
         });
     };
-  }, []); // Asegúrate de que el efecto se vuelva a ejecutar cuando los datos cambien
+  }, []);
 
   const transformedData = transformData(data);
 
   return (
     <div className="container">
       <h1>Informe de estadísticas</h1>
-      <div className='chart'>
-        <PieChart width={400} height={400}>
-          <Pie
-            dataKey="value"
+
+      <div className="chartContainer">
+        <div className='chart'>
+          <PieChart width={400} height={400}>
+            <Pie
+              dataKey="value"
+              data={transformedData}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              fill="#8884d8"
+            >
+              {transformedData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index]} />
+              ))}
+            </Pie>
+            <Legend />
+            <Tooltip />
+          </PieChart>
+          <div className="voters-count">
+            Cantidad de votantes: {totalVoters}
+          </div>
+        </div>
+        <div className='chart'>
+          <BarChart
+            width={700}
+            height={400}
             data={transformedData}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            fill="#8884d8"
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
           >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="value" fill="#8884d8">
             {transformedData.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index]} />
+              <Cell key={`cell-${index}`} fill={COLORS[index]} /> // Asigna colores personalizados
             ))}
-          </Pie>
-          <Legend />
-          <Tooltip />
-        </PieChart>
+            <Legend />
+            </Bar>
+          </BarChart>
+          <div className="voters-count">
+            Cantidad de votantes: {totalVoters}
+          </div>
+        </div>
       </div>
+     
     </div>
   );
 }
