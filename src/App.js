a424e55c3e52ab435aa871fbb1550973b5cf1392
Jecs <john024996@gmail.com>
2023-11-08 13:22:35 -0400

    remove unnecesary connection from app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
 import './index.css';
 import Chart from './pages/Chart';
-import { HubConnectionBuilder } from '@microsoft/signalr';
 function App() {
 
-  const baseUrl = "https://localhost:44361/votingsStream";
-  const connection = new HubConnectionBuilder()
-          .withUrl(baseUrl)
-          .build();
-  connection.start()
   return (
     <div className="App">
      {<Chart/>}
